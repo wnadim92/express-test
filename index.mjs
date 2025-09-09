e24317ec0468d112bb68c975a3efd7822d8f9aff
Samuel Nadim <wnadim2992@gmail.com>
2025-09-09 21:29:11 +0000

    add start

---
+++ index.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import os from 'os';
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    const helloMessage = `Peace from ${os.hostname()}`;
+    const now = new Date().toLocaleString('en-US', {
+        timeZone: 'America/New_York',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true,
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+    });
+    console.log(`${helloMessage} at ${now} EST`);
+    res.send(`${helloMessage} at ${now} EST\n`);
+})
+
+app.listen(PORT, () => {
+    console.log(`Web server is listening at port ${PORT} EST`);
+})
